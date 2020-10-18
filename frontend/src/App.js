37b00199b3fd53444ef7dc1d4c62afeab613b978
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-10-18 12:18:57 +0530

    Created New Repo

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomeScreen} />
+          <Route
+            exact
+            path="/product/:id"
+            component={ProductScreen}
+          />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
