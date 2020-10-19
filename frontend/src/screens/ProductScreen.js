4e63e7b856cf1bb1675ab79c117fe51ac3860020
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-10-19 14:15:49 +0530

    ES6 Embed

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,8 @@
-import React, { Fragment } from "react";
+import React, {
+  Fragment,
+  useEffect,
+  useState,
+} from "react";
 import {
   Button,
   Card,
@@ -9,12 +13,21 @@ import {
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find(
-    (p) => p._id === match.params.id
-  );
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(
+        `/api/products/${match.params.id}`
+      );
+      setProduct(data);
+    };
+    fetchProduct();
+  }, []);
+
   return (
     <Fragment>
       <Link className="btn btn-light my-3" to="/">
