5106d0de5c7514f5fbf4116b3000c98ff4f30291
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-10-20 14:15:25 +0530

    Moving to Shopping Card

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+} from "../constants/productConstants";
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+
+    const { data } = await axios.get("/api/products");
+
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (e) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        e.resoponse && e.response.data.message
+          ? e.resoponse.data.message
+          : e.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (
+  dispatch
+) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (e) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        e.resoponse && e.response.data.message
+          ? e.resoponse.data.message
+          : e.message,
+    });
+  }
+};
