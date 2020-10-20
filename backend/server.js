cc07dc2e51a49812f4759033ffc86f9d895b94db
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-10-20 23:24:05 +0530

    Some error

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import {
   errorHandler,
 } from "./middleware/errorMiddleware.js";
 
+import userRoutes from "./routes/userRoute.js";
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
@@ -15,10 +16,13 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 
 app.use(notFound);
