bb406068d253a3480e1e7eb44d3d8e9ba27199e9
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-10-21 16:37:07 +0530

    Some Error Troubling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import shippingScreen from "./screens/ShippingScreen";
 
 const App = () => {
   return (
@@ -19,32 +20,25 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route exact path="/" component={HomeScreen} />
           <Route
-            exact
-            path="/product/:id"
-            component={ProductScreen}
+            path="/shipping"
+            component={shippingScreen}
           />
+          <Route path="/login" component={LoginScreen} />
           <Route
-            exact
-            path="/cart/:id?"
-            component={CartScreen}
-          />
-          <Route
-            exact
-            path="/login"
-            component={LoginScreen}
-          />
-          <Route
-            exact
             path="/register"
             component={RegisterScreen}
           />
           <Route
-            exact
             path="/profile"
             component={ProfileScreen}
           />
+          <Route
+            path="/product/:id"
+            component={ProductScreen}
+          />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route exact path="/" component={HomeScreen} />
         </Container>
       </main>
       <Footer />
