79a8027a5bbd86e677a622f8d1887b5be0e164b3
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-10-21 16:37:42 +0530

    Error Resolved

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -5,7 +5,7 @@ import { LinkContainer } from "react-router-bootstrap";
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
     <Nav className="justify-content-center mb-4">
-      <Nav.item>
+      <Nav.Item>
         {step1 ? (
           <LinkContainer to="/login">
             <Nav.Link>Sign In</Nav.Link>
@@ -13,9 +13,9 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
         ) : (
           <Nav.Link disabled>Sign In</Nav.Link>
         )}
-      </Nav.item>
+      </Nav.Item>
 
-      <Nav.item>
+      <Nav.Item>
         {step2 ? (
           <LinkContainer to="/shipping">
             <Nav.Link>Shipping</Nav.Link>
@@ -23,9 +23,9 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
         ) : (
           <Nav.Link disabled>Shipping</Nav.Link>
         )}
-      </Nav.item>
+      </Nav.Item>
 
-      <Nav.item>
+      <Nav.Item>
         {step3 ? (
           <LinkContainer to="/payment">
             <Nav.Link>Payment</Nav.Link>
@@ -33,9 +33,9 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
         ) : (
           <Nav.Link disabled>Payment</Nav.Link>
         )}
-      </Nav.item>
+      </Nav.Item>
 
-      <Nav.item>
+      <Nav.Item>
         {step4 ? (
           <LinkContainer to="/placeorder">
             <Nav.Link>Place Order</Nav.Link>
@@ -43,7 +43,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
         ) : (
           <Nav.Link disabled>Place Order</Nav.Link>
         )}
-      </Nav.item>
+      </Nav.Item>
     </Nav>
   );
 };
